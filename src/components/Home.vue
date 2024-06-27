20c94eebe87b4c198013406c10e7a64e5db9a2bc
phillippa1 <157726678+phillippa1@users.noreply.github.com>
2024-06-27 17:11:00 +0100

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,8 @@
+<template>
+<h1>Hello User, Welcome to the Homepage</h1>
+</template>
+<script>
+export default{
+    name: 'Home'
+}
+</script>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,66 @@
+<template>
+<img class="logo" src="../assets/logoBooks.jpg" />
+<h1>Sign Up</h1>
+<div class="register">
+    <input type="text" v-model="name" placeholder="Enter Name"/>
+    <input type="text" v-model="email" placeholder="Enter Email"/>
+    <input type="Password" v-model="password" placeholder="Enter Password"/>
+    <button v-on:click="signUp">Sign Up</button>
+</div>
+</template>
+<script>
+import axios from 'axios'
+export default{
+    name: 'SignUp',
+    data()
+    {
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        async signUp()
+        {
+        let result = await axios.post("http://localhost:3000/users",{
+            name:this.name,
+            email:this.email,
+            password:this.password
+        });
+
+        console.warn(result);
+        if(result.status==201)
+        {
+            alert("sign-up done");
+            localStorage.setItem("user-info",JSON.stringify(result.data))
+        }
+        }
+    }
+}
+</script>
+
+<style>
+.logo{
+    width:100px
+}
+.register input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-right: auto;
+    margin-left: auto;
+    border: 1px solid skyblue;
+}
+.register button{
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue;
+    color: white;
+    cursor: pointer;
+}
+</style>
+
